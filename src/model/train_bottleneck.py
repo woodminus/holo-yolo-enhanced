7cf6021393e2b395bd124786a0c473ce85d14a9a
woodminus <160528767+woodminus@users.noreply.github.com>
2020-05-08 11:40:56 +0000

    fix trailing whitespace

--- src/model/train_bottleneck.py
+++ src/model/train_bottleneck.py
@@ -24,4 +24,22 @@ def _main():
 
     input_shape = (416,416) # multiple of 32, hw
 
-    model, bottleneck_model, last_layer_model = create_model(inpu
+    model, bottleneck_model, last_layer_model = create_model(input_shape, anchors, num_classes,
+            freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
+
+    logging = TensorBoard(log_dir=log_dir)
+    checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
+        monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
+    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
+
+    val_split = 0.1
+    with open(annotation_path) as f:
+        lines = f.readlines()
+    np.random.seed(10101)
+    np.random.shuffle(lines)
+    np.random.seed(None)
+    num_val = int(len(lines)*val_split)
+    num_train = len(lines) - num_val
+
+    
