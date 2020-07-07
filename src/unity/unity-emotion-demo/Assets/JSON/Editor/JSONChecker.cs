c31ce0ca91ed4534fc1a6a6ad18328324f3ac7c5
woodminus <160528767+woodminus@users.noreply.github.com>
2020-07-07 18:13:11 +0000

    eCryptfs: Revert to a writethrough cache model

---
+++ src/unity/unity-emotion-demo/Assets/JSON/Editor/JSONChecker.cs
@@ -0,0 +1,6 @@
+//#define PERFTEST        //For testing performance of parse/stringify.  Turn on editor profiling to see how we're doing
+
+using UnityEngine;
+using UnityEditor;
+
+public class JSON
