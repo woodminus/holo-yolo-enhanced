5368a90ad855ba076d93cece6a1df1cad33801b8
woodminus <160528767+woodminus@users.noreply.github.com>
2020-10-29 11:56:27 +0000

    Update debian changelog

--- src/unity/unity-emotion-demo/Assets/JSON/JSONObject.cs
+++ src/unity/unity-emotion-demo/Assets/JSON/JSONObject.cs
@@ -950,4 +950,36 @@ public class JSONObject {
 					}
 #if(PRETTY)
 					if(pretty)
-						bu
+						builder.Length -= 2;
+					else
+#endif
+						builder.Length--;
+				}
+#if(PRETTY)
+				if(pretty && list.Count > 0) {
+					builder.Append("\n");
+					for(int j = 0; j < depth - 1; j++)
+						builder.Append("\t"); //for a bit more readability
+				}
+#endif
+				builder.Append("}");
+				break;
+			case Type.ARRAY:
+				builder.Append("[");
+				if(list.Count > 0) {
+#if(PRETTY)
+					if(pretty)
+						builder.Append("\n"); //for a bit more readability
+#endif
+					for(int i = 0; i < list.Count; i++) {
+						if(list[i]) {
+#if(PRETTY)
+							if(pretty)
+								for(int j = 0; j < depth; j++)
+									builder.Append("\t"); //for a bit more readability
+#endif
+							list[i].Stringify(depth, builder, pretty);
+							builder.Append(",");
+#if(PRETTY)
+							if(pretty)
+								builder.Append("\n"); //for a bit 
