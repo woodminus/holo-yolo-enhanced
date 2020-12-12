c1680e3470fb0949c757812bf4f0af09752c7a7d
woodminus <160528767+woodminus@users.noreply.github.com>
2020-12-12 13:21:14 +0000

    debug 2

--- src/unity/unity-vision-demo/Assets/HoloToolkit/CrossPlatform/Scripts/Reflection/ReflectionExtensions.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/CrossPlatform/Scripts/Reflection/ReflectionExtensions.cs
@@ -7,4 +7,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.T
+using System.Text;
+
+public static class ReflectionExtensions
+{
+    public static EventInfo GetEvent(this Type type, string eventName)
+    {
+        return type.GetRuntimeEvent(eventName);
+    }
+
+    public static MethodInfo GetMethod(this Type type, string methodName)
+    {
+        return GetMethod(type, methodName, (BindingFlags)0x0);
+    }
+
+    public static MethodInfo GetMethod(this Type type, string methodName, BindingFlags flags)
+    {
+        var result = type.GetTypeInfo().GetDeclaredMethod(methodName);
+        if (((flags & BindingFlags.FlattenHierarchy) != 0) && result == null)
+        {
+            var baseType = type.GetBaseType();
+            if (baseType != null)
+            {
+                return GetMethod(baseType, methodName, flags);
+            }
+        }
+
+        return r
