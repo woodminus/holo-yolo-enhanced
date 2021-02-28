262cbbf47f41caf72f95649743357bddf96630ee
woodminus <160528767+woodminus@users.noreply.github.com>
2021-02-28 16:53:03 +0000

    workqueue: swap set_cpus_allowed_ptr() and PF_NO_SETAFFINITY

--- src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/GestureManager.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/GestureManager.cs
@@ -1,4 +1,23 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using U
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+using System.Collections.Generic;
+
+namespace HoloToolkit.Unity
+{
+    /// <summary>
+    /// GestureManager provides access to several different input gestures, including
+    /// Tap and Manipulation.
+    /// </summary>
+    /// <remarks>
+    /// When a tap gesture is detected, GestureManager uses GazeManager to find the game object.
+    /// GestureManager then sends a message to that game object.
+    /// 
+    /// Using Manipulation requires subscribing the the ManipulationStarted events and then querying
+    /// information about the manipulation gesture via ManipulationOffset and ManipulationHandPosition
+    /// </remarks>
+    [RequireComponent(typeof(GazeManager))]
+    public partial class GestureManager : Singleton<GestureManager>
+ 
