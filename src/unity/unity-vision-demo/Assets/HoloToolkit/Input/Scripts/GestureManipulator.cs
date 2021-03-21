ea61f33b95bc12eae7f9a08f2c394132e26559f5
woodminus <160528767+woodminus@users.noreply.github.com>
2021-03-21 22:17:20 +0000

    Removed minimum-stability and prefer-stable

--- src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/GestureManipulator.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/GestureManipulator.cs
@@ -1,1 +1,17 @@
-﻿// Copyright (c) Microsoft 
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    /// <summary>
+    /// A component for moving an object via the GestureManager manipulation gesture.
+    /// </summary>
+    /// <remarks>
+    /// When an active GestureManipulator component is attached to a GameObject it will subscribe
+    /// to GestureManager's manipulation gestures, and move the GameObject when a ManipulationGesture occurs.
+    /// If the GestureManipulator is disabled it will not respond to any manipulation gestures.
+    /// 
+    /// This means that if multiple GestureManipulators are active in a given scene when a manipulation
+    /// gesture is performed, all the releva
