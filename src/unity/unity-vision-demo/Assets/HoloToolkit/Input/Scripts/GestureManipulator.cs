74cdfa7f6b93c323da58dde4ab3d5d1725b5f7f1
woodminus <160528767+woodminus@users.noreply.github.com>
2021-03-22 14:21:34 +0000

    Small fix

--- src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/GestureManipulator.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/GestureManipulator.cs
@@ -14,4 +14,22 @@ namespace HoloToolkit.Unity
     /// If the GestureManipulator is disabled it will not respond to any manipulation gestures.
     /// 
     /// This means that if multiple GestureManipulators are active in a given scene when a manipulation
-    /// gesture is performed, all the releva
+    /// gesture is performed, all the relevant GameObjects will be moved.  If the desired behavior is that only
+    /// a single object be moved at a time, it is recommended that objects which should not be moved disable
+    /// their GestureManipulators, then re-enable them when necessary (e.g. the object is focused).
+    /// </remarks>
+    public class GestureManipulator : MonoBehaviour
+    {
+        [Tooltip("How much to scale each axis of hand movement (camera relative) when manipulating the object")]
+        public Vector3 handPositionScale = new Vector3(2.0f, 2.0f, 4.0f);  // Default tuning values, expected to be modified per application
+
+        private Vector3 initialHandPosition;
+
+        private Vector3 initialObjectPosition;
+
+        private Interpolator targetInterpolator;
+
+        private GestureManager gestureManager;
+
+        private bool Manipulating { get; set; }
+
