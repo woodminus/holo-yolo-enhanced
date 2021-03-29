8cdf36aa087e50639eb7c0df4c7bd6d5804bd67a
woodminus <160528767+woodminus@users.noreply.github.com>
2021-03-29 05:46:26 +0000

    Portuguese update

--- src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/GestureManipulator.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/GestureManipulator.cs
@@ -119,4 +119,7 @@ namespace HoloToolkit.Unity
                 {
                     transform.position = worldObjectPosition;
                 }
-    
+            }
+        }
+    }
+}
---
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/HandsManager.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using UnityEngine.VR.WSA.Input;
+
+namespace HoloToolkit.Unity
+{
+    /// <summary>
+    /// HandsManager determines if the hand is currently detected or not.
+    /// </summary>
+    public partial class HandsManager : Sing
