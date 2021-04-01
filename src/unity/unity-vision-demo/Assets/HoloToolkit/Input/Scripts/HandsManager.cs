c46b1e25a90605d9f6b2e294bdb91a91b41c75bd
woodminus <160528767+woodminus@users.noreply.github.com>
2021-04-01 05:55:43 +0000

    Updated Vietnamese translation

--- src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/HandsManager.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/HandsManager.cs
@@ -9,4 +9,27 @@ namespace HoloToolkit.Unity
     /// <summary>
     /// HandsManager determines if the hand is currently detected or not.
     /// </summary>
-    public partial class HandsManager : Sing
+    public partial class HandsManager : Singleton<HandsManager>
+    {
+        /// <summary>
+        /// HandDetected tracks the hand detected state.
+        /// Returns true if the list of tracked hands is not empty.
+        /// </summary>
+        public bool HandDetected
+        {
+            get { return trackedHands.Count > 0; }
+        }
+
+        /// <summary>
+        /// Occurs when users hand is detected or lost.
+        /// </summary>
+        /// <param name="handDetected">True if a hand is Detected, else false.</param>
+        public delegate void HandInViewDelegate(bool handDetected);
+        public event HandInViewDelegate HandInView;
+
+        private HashSet<uint> trackedHands = new HashSet<uint>();
+
+        private void Awake()
+        {
+            InteractionManager.SourceDetected += InteractionManager_SourceDetected;
+            InteractionManager.SourceLost += In
