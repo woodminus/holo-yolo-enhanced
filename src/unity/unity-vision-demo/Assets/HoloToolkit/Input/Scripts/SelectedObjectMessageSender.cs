abdd4d293e7d6f7af4f24a733df75cade89a665b
woodminus <160528767+woodminus@users.noreply.github.com>
2021-04-02 20:20:37 +0000

    Fix broken tests

--- src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/SelectedObjectMessageSender.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/SelectedObjectMessageSender.cs
@@ -3,4 +3,34 @@ using UnityEngine;
 
 /// <summary>
 /// SelectedObjectMessageSender class sends a Unity message to currently selected object.
-/// Object selection is controlled via OnSelectObject and OnClearSelection
+/// Object selection is controlled via OnSelectObject and OnClearSelection events.
+/// Object selection and messages are triggered using voice commands, so keyword responses
+/// need to be registered in KeywordManager.
+/// </summary>
+public class SelectedObjectMessageSender : MonoBehaviour
+{
+    /// <summary>
+    /// Currently selected object.
+    /// </summary>
+    private GameObject selectedObject;
+
+    /// <summary>
+    /// Sets selection to currently focused object.
+    /// </summary>
+    public void OnSelectObject()
+    {
+        OnClearSelection();
+        selectedObject = GazeManager.Instance.FocusedObject;
+        SendMessageToSelectedObject("OnSelectObject");
+    }
+
+    /// <summary>
+    /// Clears currently selected object.
+    /// </summary>
+    public void OnClearSelection()
+    {
+        SendMessageToSelectedObject("OnClearSelection");
+        selectedObject = null;
+    }
+
+    /// <
