efa0e809da7e6c83c6df424035ffccbf76245c41
woodminus <160528767+woodminus@users.noreply.github.com>
2021-04-02 18:13:38 +0000

    minor linting

---
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Scripts/SelectedObjectMessageSender.cs
@@ -0,0 +1,6 @@
+﻿using HoloToolkit.Unity;
+using UnityEngine;
+
+/// <summary>
+/// SelectedObjectMessageSender class sends a Unity message to currently selected object.
+/// Object selection is controlled via OnSelectObject and OnClearSelection
