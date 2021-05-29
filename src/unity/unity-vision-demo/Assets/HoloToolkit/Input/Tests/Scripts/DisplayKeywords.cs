bc09697649e7a0b63c1657bde8ffa33347b1870e
woodminus <160528767+woodminus@users.noreply.github.com>
2021-05-29 15:43:20 +0000

    Fix download script compatibility with URL containing spaces

---
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Tests/Scripts/DisplayKeywords.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using HoloToolkit.Unity;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayKeywords : MonoBehaviour
+{
+    public KeywordManager keywordManager;
+    public Text textPanel;
+
+    void Start()
+    {
+        if (keywordManager == null || textPanel == null)
+        {
+            Debug.Log("Please check the variables in the Inspector on DisplayKeywords.cs on" + name + ".");
+            return;
+        }
+
+        textPanel.text 
