c64240dd11c4167e2a3a72a16df91ff8a2828bfa
woodminus <160528767+woodminus@users.noreply.github.com>
2021-05-29 17:47:31 +0000

    Bump version to 0.0.1.

--- src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Tests/Scripts/MicStreamDemo.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Tests/Scripts/MicStreamDemo.cs
@@ -8,4 +8,27 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
 using UnityEngine;
-using Unity
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using System.Text;
+
+namespace HoloToolkit.Unity
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class MicStreamDemo : MonoBehaviour
+    {
+        /// <summary>
+        /// Which type of microphone/quality to access
+        /// </summary>
+        public MicStream.StreamCategory StreamType = MicStream.StreamCategory.HIGH_QUALITY_VOICE;
+
+        /// <summary>
+        /// can boost volume here as desired. 1 is default but probably too quiet. can change during operation. 
+        /// </summary>
+        public float InputGain = 1;
+
+        /// <summary>
+        /// if keepAllData==false, you'll always get the newest data no matter how long the program hangs for any reason, but will lose some data if the program does hang 
+        /// can only be set on initialization
+        /// </summary>
+        public 
