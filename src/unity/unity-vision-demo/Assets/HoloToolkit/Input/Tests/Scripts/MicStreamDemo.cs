04e3f10489d0429bd2fab88d37c7ca7639a01f62
woodminus <160528767+woodminus@users.noreply.github.com>
2021-05-30 08:17:29 +0000

    minor reformat

--- src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Tests/Scripts/MicStreamDemo.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Tests/Scripts/MicStreamDemo.cs
@@ -31,4 +31,27 @@ namespace HoloToolkit.Unity
         /// if keepAllData==false, you'll always get the newest data no matter how long the program hangs for any reason, but will lose some data if the program does hang 
         /// can only be set on initialization
         /// </summary>
-        public 
+        public bool KeepAllData = false;
+
+        /// <summary>
+        /// Should the mic stream start automatically when this component is enabled?
+        /// </summary>
+        public bool AutomaticallyStartStream = true;
+
+        /// <summary>
+        /// Do you want to hear what the microphone sounds like by listening to the AudioSource in Unity?
+        /// </summary>
+        public bool ListenToAudioSource = true;
+
+        /// <summary>
+        /// The name of the file to which to save audio (for commands that save to a file)
+        /// </summary>
+        public string SaveFileName = "MicrophoneTest.wav";
+
+        /// <summary>
+        /// Records estimation of volume from the microphone to affect other elements of the game object
+        /// </summary>
+        private float averageAmplitude = 0;
+
+        /// <summary>
+        
