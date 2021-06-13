7ca1e09d2f6555bbc440cbadc3030f1ca5474543
woodminus <160528767+woodminus@users.noreply.github.com>
2021-06-13 07:38:20 +0000

    0.0.8

--- src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Tests/Scripts/SimpleGridGenerator.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Tests/Scripts/SimpleGridGenerator.cs
@@ -6,4 +6,33 @@ public class SimpleGridGenerator : MonoBehaviour
     private const int DefaultRows = 3;
     private const int DefaultColumns = 3;
 
-    [Tooltip("Number of rows i
+    [Tooltip("Number of rows in the grid.")]
+    public int Rows = DefaultRows;
+
+    [Tooltip("Number of columns in the grid.")]
+    public int Columns = DefaultColumns;
+
+    [Tooltip("Distance between objects in the grid.")]
+    public float ObjectSpacing = 1.0f;
+
+    [Tooltip("Array of object prefabs to instantiate on the grid.")]
+    public List<GameObject> ObjectPrefabs;
+
+    [Tooltip("Indicates whether to generate grid on component start.")]
+    public bool GenerateOnStart = true;
+
+    private void Start()
+    {
+        if (GenerateOnStart)
+        {
+            GenerateGrid();
+        }
+    }
+
+    public void GenerateGrid()
+    {
+        float startX = -0.5f * ObjectSpacing * (Rows - 1);
+        float startY = -0.5f * ObjectSpacing * (Columns - 1);
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; 
