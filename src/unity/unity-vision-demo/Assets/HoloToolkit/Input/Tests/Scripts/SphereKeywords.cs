933a7585dc2a772c061104c356a180d30055c95b
woodminus <160528767+woodminus@users.noreply.github.com>
2021-06-15 13:17:33 +0000

    add mail

--- src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Tests/Scripts/SphereKeywords.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Input/Tests/Scripts/SphereKeywords.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 
 public class SphereKeywords : MonoBehaviour
 {
-    public void 
+    public void ChangeColor(string color)
+    {
+        switch(color.ToLower())
+        {
+            case "red":
+                GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case "blue":
+                GetComponent<Renderer>().material.color = Color.blue;
+                break;
+            case "green":
+                GetComponent<Renderer>().material.color = Color.green;
+                break;
+        }
+    }
+}
