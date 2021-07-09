cc34870ea827f36fa2ac4d46c1370a804f74f953
woodminus <160528767+woodminus@users.noreply.github.com>
2021-07-09 17:18:39 +0000

    Update meetings.md

--- src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialMapping/Scripts/ObjectSurfaceObserver.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialMapping/Scripts/ObjectSurfaceObserver.cs
@@ -7,4 +7,31 @@ namespace HoloToolkit.Unity
 {
     public class ObjectSurfaceObserver : SpatialMappingSource
     {
-        [Tooltip("The roo
+        [Tooltip("The room model to use when loading meshes in Unity.")]
+        public GameObject roomModel;
+
+        // Use this for initialization.
+        private void Start()
+        {
+#if UNITY_EDITOR
+            // When in the Unity editor, try loading saved meshes from a model.
+            Load(roomModel);
+
+            if (GetMeshFilters().Count > 0)
+            {
+                SpatialMappingManager.Instance.SetSpatialMappingSource(this);
+            }
+#endif
+        }
+
+        /// <summary>
+        /// Loads the SpatialMapping mesh from the specified room object.
+        /// </summary>
+        /// <param name="roomModel">The room model to load meshes from.</param>
+        public void Load(GameObject roomModel)
+        {
+            if (roomModel == null)
+            {
+                Debug.Log("No room model specified.");
+                return;
+    
