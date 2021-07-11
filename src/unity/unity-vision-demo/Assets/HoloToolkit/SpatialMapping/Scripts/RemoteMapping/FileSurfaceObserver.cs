df54dc717e34afcfc3cbdb4f506bb8dff1c9f2bf
woodminus <160528767+woodminus@users.noreply.github.com>
2021-07-11 14:06:36 +0000

    Update manual

--- src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialMapping/Scripts/RemoteMapping/FileSurfaceObserver.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialMapping/Scripts/RemoteMapping/FileSurfaceObserver.cs
@@ -8,4 +8,27 @@ namespace HoloToolkit.Unity
 {
     public class FileSurfaceObserver : SpatialMappingSource
     {
-        [Tooltip("The f
+        [Tooltip("The file name to use when saving and loading meshes.")]
+        public string MeshFileName = "roombackup";
+
+        [Tooltip("Key to press in editor to load a spatial mapping mesh from a .room file.")]
+        public KeyCode LoadFileKey = KeyCode.L;
+
+        [Tooltip("Key to press in editor to save a spatial mapping mesh to file.")]
+        public KeyCode SaveFileKey = KeyCode.S;
+
+        /// <summary>
+        /// Loads the SpatialMapping mesh from the specified file.
+        /// </summary>
+        /// <param name="fileName">The name, without path or extension, of the file to load.</param>
+        public void Load(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                Debug.Log("No mesh file specified.");
+                return;
+            }
+
+            Cleanup();
+
+            List<Mesh> storedM
