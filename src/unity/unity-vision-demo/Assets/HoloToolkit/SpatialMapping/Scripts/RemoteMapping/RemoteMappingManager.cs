86700ac199a9f268db353d5c085a6608f68c606b
woodminus <160528767+woodminus@users.noreply.github.com>
2021-07-14 18:52:32 +0000

    libraries

---
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialMapping/Scripts/RemoteMapping/RemoteMappingManager.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+namespace HoloToolkit.Unity
+{
+    [RequireComponent(typeof(RemoteMeshTarget))]
+    public partial class RemoteMappingManager : Singleton<RemoteMappingManager>
+    {
+        [Tooltip("Key to press in editor to enable spatial mapping over the network.")]
+        public KeyCode RemoteMappingKey = KeyCode.N;
+
+        [Tooltip("Keyword for sending meshes from HoloLens to Unity over the network.")]
+        public string SendMe
