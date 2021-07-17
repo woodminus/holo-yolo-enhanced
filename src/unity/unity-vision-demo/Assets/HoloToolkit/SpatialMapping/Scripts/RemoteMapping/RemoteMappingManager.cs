6a07f7371496efac4726ecc30a5ec2b0b00a84f7
woodminus <160528767+woodminus@users.noreply.github.com>
2021-07-17 13:25:56 +0000

    add logo image

--- src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialMapping/Scripts/RemoteMapping/RemoteMappingManager.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialMapping/Scripts/RemoteMapping/RemoteMappingManager.cs
@@ -15,4 +15,29 @@ namespace HoloToolkit.Unity
         public KeyCode RemoteMappingKey = KeyCode.N;
 
         [Tooltip("Keyword for sending meshes from HoloLens to Unity over the network.")]
-        public string SendMe
+        public string SendMeshesKeyword = "send meshes";
+
+        /// <summary>
+        /// Receives meshes collected over the network.
+        /// </summary>
+        private RemoteMeshTarget remoteMeshTarget;
+
+        /// <summary>
+        /// Used for voice commands.
+        /// </summary>
+        private KeywordRecognizer keywordRecognizer;
+
+        /// <summary>
+        /// Collection of supported keywords and their associated actions.
+        /// </summary>
+        private Dictionary<string, System.Action> keywordCollection;
+
+        // Use this for initialization.
+        private void Start()
+        {
+            // Create our keyword collection.
+            keywordCollection = new Dictionary<string, System.Action>();
+            keywordCollection.Add(SendMeshesKeyword, () => SendMeshes());
+
+            // Tell the KeywordRecognizer about our keywords.
+   
