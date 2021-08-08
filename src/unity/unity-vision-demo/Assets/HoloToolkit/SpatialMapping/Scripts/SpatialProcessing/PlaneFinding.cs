891c7a5867d4d2540668968456c0a5ee2bc2fde4
woodminus <160528767+woodminus@users.noreply.github.com>
2021-08-08 07:48:27 +0000

    Update API Builder Models

---
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialMapping/Scripts/SpatialProcessing/PlaneFinding.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct OrientedBoundingBox
+    {
+        public Vector3 Center;
+        public Vector3 Extents;
+        public Quaternion Rotation;
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct BoundedPlane
+    {
+        public Plane Plane;
+        public Or
