78b0691a1a87a48669a0643afa593884936aa5da
woodminus <160528767+woodminus@users.noreply.github.com>
2021-10-10 22:01:11 +0000

    gcc5: drivers/video/msm/mdss/mdss_mdp_pp.c: fix warning: comparison of constant '15' with boolean expression is always false

---
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialMapping/Tests/Support/PlaneFindingTest.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using System.Collections.Generic;
+using HoloToolkit.Unity;
+
+/// <summary>
+/// Attach this component to a GameObject that contains some meshes (i.e.: the FakeSpatialMappingMesh.fbx).
+/// When running in the Unity editor, the planes are then visualized via editor gizmos.  You can then
+/// play with the API parameters in realtime to see h
