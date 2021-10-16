64bec8f740e78488e56941e38522568f080e2bb5
woodminus <160528767+woodminus@users.noreply.github.com>
2021-10-16 19:03:19 +0000

    Update Chapter7/Ex1.md

--- src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialMapping/Tests/Support/PlaneFindingTest.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialMapping/Tests/Support/PlaneFindingTest.cs
@@ -8,4 +8,30 @@ using HoloToolkit.Unity;
 /// <summary>
 /// Attach this component to a GameObject that contains some meshes (i.e.: the FakeSpatialMappingMesh.fbx).
 /// When running in the Unity editor, the planes are then visualized via editor gizmos.  You can then
-/// play with the API parameters in realtime to see h
+/// play with the API parameters in realtime to see how the impact the plane finding algorithm.
+/// </summary>
+public class PlaneFindingTest : MonoBehaviour
+{
+    [Range(0, 45)]
+    public float SnapToGravityThreshold = 0.0f;
+
+    [Range(0,10)]
+    public float MinArea = 1.0f;
+
+    public bool VisualizeSubPlanes = false;
+
+    private List<PlaneFinding.MeshData> meshData = new List<PlaneFinding.MeshData>();
+    private BoundedPlane[] planes;
+
+    private void Update()
+    {
+        // Grab the necessary mesh data from the current set of surfaces that we want to run
+        // PlaneFinding against.  This must be done on the main UI thread.
+        meshData.Clear();
+        foreach (MeshFilter mesh in GetComponentsInChildren<MeshFilter>())
+        {
+            meshData.Add(new PlaneFinding.MeshData(mesh));
+        }
+
+        // Now call FindPlanes().  NOTE: In a real application, this MUST be executed on a
+        // background thread (i.
