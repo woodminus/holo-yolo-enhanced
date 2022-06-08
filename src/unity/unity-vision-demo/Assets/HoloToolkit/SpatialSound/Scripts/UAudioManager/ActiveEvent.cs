957fdc2ea1e50d04016d6f33bb4ef1376e1f0d18
woodminus <160528767+woodminus@users.noreply.github.com>
2022-06-08 05:53:08 +0000

    Added MIT Licence

---
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialSound/Scripts/UAudioManager/ActiveEvent.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    /// <summary>
+    /// Currently active AudioEvents along with their AudioSource components for instance limiting events
+    /// </summary>
+    public class ActiveEvent : IDisposable
+    {
+        private AudioSource primarySource = null;
+        public AudioSource PrimarySource
+        {
+            get
+            {
+                return primarySource;
+            }
+            private set
+            {
+                primarySource = value;
+                if (primarySource != null)
+                {
