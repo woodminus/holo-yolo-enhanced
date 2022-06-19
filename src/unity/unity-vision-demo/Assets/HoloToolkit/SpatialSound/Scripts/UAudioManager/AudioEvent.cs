851397367d2250fb5493bb2fe5ab192f88742deb
woodminus <160528767+woodminus@users.noreply.github.com>
2022-06-19 12:50:15 +0000

    home changes

--- src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialSound/Scripts/UAudioManager/AudioEvent.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialSound/Scripts/UAudioManager/AudioEvent.cs
@@ -1,2 +1,40 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for licen
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    /// <summary>
+    /// The different rules for how audio should be played back.
+    /// </summary>
+    public enum AudioContainerType
+    {
+        Random,
+        Sequence,
+        Simultaneous,
+        ContinuousSequence,
+        ContinuousRandom
+    }
+
+    /// <summary>
+    /// Defines the behavior for when the instance limit is reached for a particular event.
+    /// </summary>
+    public enum AudioEventInstanceBehavior
+    {
+        KillOldest,
+        KillNewest
+    }
+
+    /// <summary>
+    /// The different types of spatial positioning.
+    /// </summary>
+    public enum SpatialPositioningType
+    {
+        TwoD,           // Stereo
+        ThreeD,         // 3D audio
+        SpatialSound,   // Microsoft Spatial Sound
+    }
+
+    /
