5f47c6b01cdd8b3636bdd0cbf4a79cfbe56f63c3
woodminus <160528767+woodminus@users.noreply.github.com>
2022-07-20 21:04:47 +0000

    merge develop branch

---
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialSound/Scripts/UAudioManager/Editor/UAudioManagerBaseEditor.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    public class UAudioManagerBaseEditor<TEvent> : Editor where TEvent : AudioEvent, new()
+    {
+        protected UAudioManagerBase<TEvent> myTarget;
+        private string[] eventNames;
+        private int selectedEventIndex = 0;
+        private readonly string[] posTypes = { "2D", "3D", "Spatial Sound" };
+
+      
