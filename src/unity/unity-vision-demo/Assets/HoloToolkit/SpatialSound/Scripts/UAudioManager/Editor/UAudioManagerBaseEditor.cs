5922a051a7f8ed44632081c3a506e017bfa3fb83
woodminus <160528767+woodminus@users.noreply.github.com>
2022-07-27 09:09:28 +0000

    Update tower.ts

--- src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialSound/Scripts/UAudioManager/Editor/UAudioManagerBaseEditor.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialSound/Scripts/UAudioManager/Editor/UAudioManagerBaseEditor.cs
@@ -14,4 +14,25 @@ namespace HoloToolkit.Unity
         private int selectedEventIndex = 0;
         private readonly string[] posTypes = { "2D", "3D", "Spatial Sound" };
 
-      
+        protected void SetUpEditor()
+        {
+            // Having a null array of events causes too many errors and should only happen on first adding anyway.
+            if (this.myTarget.EditorEvents == null)
+            {
+                this.myTarget.EditorEvents = new TEvent[0];
+            }
+            this.eventNames = new string[this.myTarget.EditorEvents.Length];
+            UpdateEventNames(this.myTarget.EditorEvents);
+        }
+
+        protected void DrawInspectorGUI(bool showEmitters)
+        {
+            this.serializedObject.Update();
+            EditorGUI.BeginChangeCheck();
+            DrawEventHeader(this.myTarget.EditorEvents);
+
+            if (this.myTarget.EditorEvents != null && this.myTarget.EditorEvents.Length > 0)
+            {
+                // Display current event in dropdown.
+                EditorGUI.indentLevel++;
+  
