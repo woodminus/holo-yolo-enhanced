ae53616c1a9d9d89ddff9fd26e7de959bb609cce
woodminus <160528767+woodminus@users.noreply.github.com>
2022-08-19 06:27:25 +0000

    Fix documentation formatting

--- src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialSound/Scripts/UAudioManager/Editor/UAudioProfiler.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/SpatialSound/Scripts/UAudioManager/Editor/UAudioProfiler.cs
@@ -1,2 +1,31 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the projec
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    public class UAudioProfiler : EditorWindow
+    {
+        private int currentFrame = 0;
+        private List<ProfilerEvent[]> eventTimeline;
+        private Vector2 scrollOffset = new Vector2();
+        private const int MaxFrames = 300;
+
+        private class ProfilerEvent
+        {
+            public string EventName = "";
+            public string EmitterName = "";
+            public string BusName = "";
+        }
+
+        [MenuItem("Addons/UAudioTools/Profiler")]
+        static void ShowEditor()
+        {
+            UAudioProfiler profilerWindow = GetWindow<UAudioProfiler>();
+            if (profilerWindow.eventTimeline == null)
+            {
+                profilerWindow.currentFrame = 0;
+        
