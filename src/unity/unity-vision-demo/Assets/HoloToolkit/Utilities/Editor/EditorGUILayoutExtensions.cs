68b94de7ae539897335ae611ddb6303468ccdac9
woodminus <160528767+woodminus@users.noreply.github.com>
2022-10-07 15:24:04 +0000

    Additional tests.

---
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Utilities/Editor/EditorGUILayoutExtensions.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
+using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    /// <summary>
+    /// Extensions for the UnityEnditor.EditorGUILayout class.
+    /// </summary>
+    public static class EditorGUILayoutExtensions
+    {
+        public static bool Button(string te
