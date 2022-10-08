e59284e7088a5d1e72b64f15f7f306954199f2b8
woodminus <160528767+woodminus@users.noreply.github.com>
2022-10-08 23:01:03 +0000

    R Package Installation

--- src/unity/unity-vision-demo/Assets/HoloToolkit/Utilities/Editor/EditorGUILayoutExtensions.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Utilities/Editor/EditorGUILayoutExtensions.cs
@@ -11,4 +11,27 @@ namespace HoloToolkit.Unity
     /// </summary>
     public static class EditorGUILayoutExtensions
     {
-        public static bool Button(string te
+        public static bool Button(string text, params GUILayoutOption[] options)
+        {
+            return Button(text, GUI.skin.button, options);
+        }
+
+        public static bool Button(string text, GUIStyle style, params GUILayoutOption[] options)
+        {
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Space(EditorGUIExtensions.Indent);
+            bool pressed = GUILayout.Button(text, style, options);
+            EditorGUILayout.EndHorizontal();
+            return pressed;
+        }
+
+        public static void Label(string text, params GUILayoutOption[] options)
+        {
+            Label(text, EditorStyles.label, options);
+        }
+
+        public static void Label(string text, GUIStyle style, params GUILayoutOption[] options)
+        {
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Space(EditorGUIExtensions.Indent);
+ 
