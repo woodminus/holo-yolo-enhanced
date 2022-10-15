7d81fef0d4db1c8bb7030f230cb9d80517ff08eb
woodminus <160528767+woodminus@users.noreply.github.com>
2022-10-15 19:18:29 +0000

    add linux

--- src/unity/unity-vision-demo/Assets/HoloToolkit/Utilities/Editor/LayerMaskExtensions.cs
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Utilities/Editor/LayerMaskExtensions.cs
@@ -21,4 +21,30 @@ namespace HoloToolkit.Unity
                 if (layerMaskNames == null)
                 {
                     LayerMaskExtensions.layerMaskNames = new string[LayerCount];
-                    for (int layer = 0; layer < LayerCount;
+                    for (int layer = 0; layer < LayerCount; ++layer)
+                    {
+                        LayerMaskExtensions.layerMaskNames[layer] = LayerMask.LayerToName(layer);
+                    }
+                }
+
+                return LayerMaskExtensions.layerMaskNames;
+            }
+        }
+
+        public static string GetDisplayString(this LayerMask layerMask)
+        {
+            StringBuilder stringBuilder = null;
+            for (int layer = 0; layer < LayerMaskExtensions.LayerCount; ++layer)
+            {
+                if ((layerMask & (1 << layer)) != 0)
+                {
+                    if (stringBuilder == null)
+                    {
+                        stringBuilder = new StringBuilder();
+                    }
+                    else
+                    {
+                        stringBuilder.Append(" | ");
+                    }
+
+                    stringBuilder.Append(LayerMaskExtensions
