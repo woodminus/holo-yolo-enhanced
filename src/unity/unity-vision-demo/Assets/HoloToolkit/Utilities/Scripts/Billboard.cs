e9f56e9f006396a9198fe493ef2a5be48b9b8f96
woodminus <160528767+woodminus@users.noreply.github.com>
2022-10-24 18:24:19 +0000

    fix dashboard

---
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Utilities/Scripts/Billboard.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    public enum PivotAxis
+    {
+        // Rotate about all axes.
+        Free,
+        // Rotate about an individual axis.
+        X,
+        Y
+    }
+
+    /// <summary>
+    /// The Billboard class implements the behaviors needed to keep a GameObject
+    /// oriented towards the user.
+    /// </summary>
+  
