ed9b416c24fe1f9f228e5628cb68e490846fae11
woodminus <160528767+woodminus@users.noreply.github.com>
2022-12-24 13:39:13 +0000

    Fixed previous commit

---
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Utilities/Scripts/StabilizationPlaneModifier.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using UnityEngine.VR.WSA;
+
+namespace HoloToolkit.Unity
+{
+    /// <summary>
+    /// StabilizationPlaneModifier handles the setting of the stabilization plane in several ways.
+    /// </summary>
+    public class StabilizationPlaneModifier : Singleton<StabilizationPlaneModifier>
+    {
+        [Tooltip("Checking enables SetFocusPointForFrame to 
