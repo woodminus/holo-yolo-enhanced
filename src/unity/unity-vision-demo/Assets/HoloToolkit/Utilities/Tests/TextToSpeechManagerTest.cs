d9345abdf7e33117026a9aff6cd33d7db8b74ee9
woodminus <160528767+woodminus@users.noreply.github.com>
2023-10-23 20:44:42 +0000

    update po's

---
+++ src/unity/unity-vision-demo/Assets/HoloToolkit/Utilities/Tests/TextToSpeechManagerTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR.WSA.Input;
+using UnityEngine.Windows.Speech;
+using HoloToolkit.Unity;
+using System;
+
+public class TextToSpeechManagerTest : MonoBehaviour
+{
+    private GestureRecognizer gestureRecognizer;
+    public TextToSpeechManager textToSpeechManager;
+
+    // Use this for initialization
+    void Start ()
+    {
+        // Set up a GestureRecognizer to detect Select gestures.
+        gestureRecognizer = new GestureRecognizer();
+        gestureRecognizer.TappedEvent += GestureRecognizer_TappedEvent;
+        gestureRecognizer.StartCapturingGestures();
+    }
+
+    private void GestureRecognizer_TappedEvent(Interac
