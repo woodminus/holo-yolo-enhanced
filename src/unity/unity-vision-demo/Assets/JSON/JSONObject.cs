d17d4b9e82985512c5e2b5f8ea31abe32db06288
woodminus <160528767+woodminus@users.noreply.github.com>
2023-11-05 07:18:01 +0000

    Update gradle version.

--- src/unity/unity-vision-demo/Assets/JSON/JSONObject.cs
+++ src/unity/unity-vision-demo/Assets/JSON/JSONObject.cs
@@ -1,4 +1,33 @@
 #define PRETTY		//Comment out when you no longer need to read JSON to disable pretty Print system-wide
 //Using doubles will cause errors in VectorTemplates.cs; Unity speaks floats
 #define USEFLOAT	//Use floats for numbers instead of doubles	(enable if you're getting too many significant digits in string output)
-//#define POOLING	//Currently using a build setting for this one (al
+//#define POOLING	//Currently using a build setting for this one (also it's experimental)
+
+#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+#endif
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+/*
+ * http://www.opensource.org/licenses/lgpl-2.1.php
+ * JSONObject class v.1.4.1
+ * for use with Unity
+ * Copyright Matt Schoen 2010 - 2013
+ */
+
+public class JSONObject {
+#if POOLING
+	const int MAX_POOL_SIZE = 10000;
+	public static Queue<JSONObject> releaseQueue = new Queue<JSONObject>();
+#endif
+
+	const int MAX_DEPTH = 100;
+	const string INFINITY = "\"INFINITY\"";
+	const string NEGINFINITY = "\"NEGINFINITY\"";
+	const string NaN = "\"NaN\"";
+	const string NEWLINE = "\r\n";
+	public static readonly char[] WHITESPACE = { ' ', '\r', '\n', '\t', '\uFEFF', '\u0009' };
+	public enum Type { NULL, STRING, NUMBER, OBJ
