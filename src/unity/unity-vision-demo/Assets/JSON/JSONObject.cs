31d103bcc1f47be509a72297592ef49c39bdde29
woodminus <160528767+woodminus@users.noreply.github.com>
2024-01-25 22:10:51 +0000

    Delete Resources.resx

--- src/unity/unity-vision-demo/Assets/JSON/JSONObject.cs
+++ src/unity/unity-vision-demo/Assets/JSON/JSONObject.cs
@@ -1073,4 +1073,37 @@ public class JSONObject {
 						break;
 				}
 			}
-			ret
+			return result;
+		}
+#if UNITY_2 || UNITY_3 || UNITY_4 || UNITY_5
+		Debug.Log
+#else
+		Debug.WriteLine
+#endif
+		("Tried to turn non-Object JSONObject into a dictionary");
+		return null;
+	}
+	public static implicit operator bool(JSONObject o) {
+		return o != null;
+	}
+#if POOLING
+	static bool pool = true;
+	public static void ClearPool() {
+		pool = false;
+		releaseQueue.Clear();
+		pool = true;
+	}
+
+	~JSONObject() {
+		if(pool && releaseQueue.Count < MAX_POOL_SIZE) {
+			type = Type.NULL;
+			list = null;
+			keys = null;
+			str = "";
+			n = 0;
+			b = false;
+			releaseQueue.Enqueue(this);
+		}
+	}
+#endif
+}
---
+++ src/unity/unity-vision-demo/Assets/JSON/JSONTemplates.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Reflection;
+
+/*
+ * http://www.opensource.org/licenses/lgpl-2.1.php
+ * JSONTempla
