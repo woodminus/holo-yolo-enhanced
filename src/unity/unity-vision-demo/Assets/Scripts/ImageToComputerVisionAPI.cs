d59ec320cf9bc7c96bbda1c14c700d75de9b64e8
woodminus <160528767+woodminus@users.noreply.github.com>
2024-03-18 15:27:50 +0000

    change method name

---
+++ src/unity/unity-vision-demo/Assets/Scripts/ImageToComputerVisionAPI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.Networking;
+using System;
+using System.Collections.Generic;
+
+public class ImageToComputerVisionAPI : MonoBehaviour {
+
+    string VISIONKEY = "YOURVISIONKEYHERE"; // replace with your Computer Vision API Key
+
+    string e
