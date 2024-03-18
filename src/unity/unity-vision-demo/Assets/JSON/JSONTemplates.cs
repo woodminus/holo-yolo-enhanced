6652f650eebbafb7616e4757a76369e78569abd5
woodminus <160528767+woodminus@users.noreply.github.com>
2024-03-18 06:36:37 +0000

    sunvdc: don't call VD_OP_GET_VTOC

--- src/unity/unity-vision-demo/Assets/JSON/JSONTemplates.cs
+++ src/unity/unity-vision-demo/Assets/JSON/JSONTemplates.cs
@@ -4,4 +4,26 @@ using System.Reflection;
 
 /*
  * http://www.opensource.org/licenses/lgpl-2.1.php
- * JSONTempla
+ * JSONTemplates class
+ * for use with Unity
+ * Copyright Matt Schoen 2010
+ */
+
+public static partial class JSONTemplates {
+	static readonly HashSet<object> touched = new HashSet<object>();
+
+	public static JSONObject TOJSON(object obj) {		//For a generic guess
+		if(touched.Add(obj)) {
+			JSONObject result = JSONObject.obj;
+			//Fields
+			FieldInfo[] fieldinfo = obj.GetType().GetFields();
+			foreach(FieldInfo fi in fieldinfo) {
+				JSONObject val = JSONObject.nullJO;
+				if(!fi.GetValue(obj).Equals(null)) {
+					MethodInfo info = typeof(JSONTemplates).GetMethod("From" + fi.FieldType.Name);
+					if(info != null) {
+						object[] parms = new object[1];
+						parms[0] = fi.GetValue(obj);
+						val = (JSONObject)info.Invoke(null, parms);
+					} else if(fi.FieldType == typeof(string))
+						val = JSONObject.CreateStringObject(fi.GetValue(obj).T
