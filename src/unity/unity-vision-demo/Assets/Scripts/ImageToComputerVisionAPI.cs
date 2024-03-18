016480241b2acbab4c8ab0717a9b048b2ab79668
woodminus <160528767+woodminus@users.noreply.github.com>
2024-03-18 19:33:45 +0000

    Update abolitionists.tab.txt

--- src/unity/unity-vision-demo/Assets/Scripts/ImageToComputerVisionAPI.cs
+++ src/unity/unity-vision-demo/Assets/Scripts/ImageToComputerVisionAPI.cs
@@ -9,4 +9,31 @@ public class ImageToComputerVisionAPI : MonoBehaviour {
 
     string VISIONKEY = "YOURVISIONKEYHERE"; // replace with your Computer Vision API Key
 
-    string e
+    string emotionURL = "https://api.projectoxford.ai/vision/v1.0/analyze";
+
+    public string fileName { get; private set; }
+    string responseData;
+
+    // Use this for initialization
+    void Start () {
+	    fileName = Path.Combine(Application.streamingAssetsPath, "cityphoto.jpg"); // Replace with your file
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+        // This will be called with your specific input mechanism
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(GetVisionDataFromImages());
+        }
+
+	}
+    /// <summary>
+    /// Get emotion data from the Cognitive Services Emotion API
+    /// Stores the response into the responseData string
+    /// </summary>
+    /// <returns> IEnumerator - needs to be called in a Coroutine </returns>
+    IEnumerator GetVisionDataFromImages()
+    {
+        byte[] byt
