42e5d9833057bbf831f47aceca84710dbed1c76e
woodminus <160528767+woodminus@users.noreply.github.com>
2024-06-01 08:51:19 +0000

    Ajout de commentaires

--- src/uwp/App2/App.xaml.cs
+++ src/uwp/App2/App.xaml.cs
@@ -45,4 +45,24 @@ namespace App2
             // just ensure that the window is active
             if (rootFrame == null)
             {
-                // Create a Frame to act as the navig
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
